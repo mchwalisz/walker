be650f127c6d399047d8e644be2da8062b64b10f
Mikołaj Chwalisz <chwalisz@tkn.tu-berlin.de>
2017-07-19 16:57:58 +0200

    full scan experiment

--- fabfile/__init__.py
+++ fabfile/__init__.py
@@ -1,6 +1,8 @@
 import time
+from itertools import permutations
 from fabric.api import *
 from fabric.exceptions import CommandTimeout
+from fabric.decorators import runs_once
 import fabfile.config as config # noqa
 import fabfile.wifi as wifi # noqa
 from fabfile.config import set_hosts  # noqa
@@ -25,3 +27,18 @@ def run_iperf():
     elif env.hosts.index(env.host_string) == 1:
         time.sleep(1)
         run('iperf -i 1 -t  10 -c {}'.format(env.hosts[0]))
+
+
+@task()
+@runs_once
+def full_scan():
+    with settings(parallel=True):
+        execute(wifi.interfaces_create)
+    for server in env.hosts:
+        print("S: {}".format(server))
+        # Setup
+        execute(wifi.create_ap, hosts=[server])
+        # Experiment
+        execute(wifi.scan, hosts=[x for x in env.hosts if x != server])
+        # Tear down
+        execute(wifi.interfaces_create, hosts=[server])
