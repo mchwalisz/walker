dd79457ba9cfd6102e38c764cf7fa18cf38a86c5
Kai Geißdörfer <kai.s.geissdoerfer@campus.tu-berlin.de>
2017-12-06 15:24:56 +0100

    click interface for creating and uploading image and rendering rspec

--- images/digest-image.py
+++ images/digest-image.py
@@ -1,37 +1,47 @@
 #!/usr/bin/env python
 
 import os
-import boto3
 import jinja2
 import yaml
 import sys
+from invoke import run
+import click
+import awss3 as cloudstorage
 
 BASE_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
 
 
-def upload_aws(filename):
+def __build(diskimage, release):
 
-    s3 = boto3.client('s3')
+    print(f'Creating "{diskimage}"..')
+    run(f'disk-image-create ubuntu-squashfs twist -t tgz -o {diskimage}',
+        env={
+             'DIB_RELEASE': release,
+             'ELEMENTS_PATH': os.path.join(
+                    os.path.dirname(__file__), 'add_elements')
+            })
+    print(f'Done creating "{diskimage}"')
 
-    bucket = "twistimages"
-    key = "image.tgz"
 
-    s3.upload_file(filename, bucket, key, ExtraArgs={'ACL': 'public-read'})
+def __upload(diskimage):
+    print(f'Uploading {diskimage} to cloud storage..')
+    url = cloudstorage.upload(diskimage)
+    print(f'Uploaded {diskimage} to cloud storage. URL: {url}')
+    return(url)
 
-    return(f"https://s3.eu-central-1.amazonaws.com/{bucket}/{key}")
 
-
-def render_rspec(image_url):
-
+def __render_rspec(url):
     with open(os.path.join(BASE_PATH, 'rspecs', 'nodes.yml'), 'r') as stream:
         node_spec = yaml.load(stream)
 
     nodes = list()
     for node_type in node_spec:
         for node_name in node_spec[node_type]:
-            node = {'name': node_name, 'type': node_type}
-            if(node_type == 'nuc'):
-                node['disk_image'] = image_url
+            node = {
+                    'name': node_name,
+                    'type': node_type,
+                    'disk_image': url
+                    }
             nodes.append(node)
 
     rspec = jinja2.Environment(
@@ -40,15 +50,46 @@ def render_rspec(image_url):
 
     output = rspec.render({'nodes': nodes})
 
-    with open(os.path.join(BASE_PATH, 'rspecs', 'rendered.rspec'), 'w') as f:
+    rspec_path = os.path.join(BASE_PATH, 'rspecs', 'rendered.rspec')
+    with open(rspec_path, 'w') as f:
         f.write(output)
 
+    print(f'Rspec written to {rspec_path}')
 
-if __name__ == "__main__":
-    if(len(sys.argv) == 1):
-        filename = "image.tgz"
+
+@click.group(invoke_without_command=True)
+@click.option('--diskimage', default='image.tgz',
+              help='Filename for diskimage')
+@click.option('--release', default='xenial', help='Ubuntu release codename')
+@click.pass_context
+def cli(ctx, diskimage, release):
+    if ctx.invoked_subcommand is None:
+        __build(diskimage, release)
+        url = __upload(diskimage)
+        __render_rspec(url)
     else:
-        filename = sys.argv[1]
+        ctx.obj['diskimage'] = diskimage
+        ctx.obj['release'] = release
 
-    image_url = upload_aws(filename)
-    render_rspec(image_url)
+
+@cli.command()
+@click.pass_context
+def build(ctx):
+    __build(ctx.obj['diskimage'], ctx.obj['release'])
+
+
+@cli.command()
+@click.pass_context
+def upload(ctx):
+    __upload(ctx.obj['diskimage'])
+
+
+@cli.command()
+@click.option('--url',
+              help='diskimage URL to bake into rspec')
+def render_rspec(url):
+    __render_rspec(url)
+
+
+if __name__ == '__main__':
+    cli(obj={})
