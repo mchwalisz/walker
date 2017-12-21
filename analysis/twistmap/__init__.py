6687d2af5d56b34f00e0ec6a45e9b625517f6d4d
Mikołaj Chwalisz <m.chwalisz@gmail.com>
2017-12-21 20:08:16 +0100

    create basic connectivity map

--- analysis/twistmap/__init__.py
+++ analysis/twistmap/__init__.py
@@ -1,90 +1,94 @@
 # import numpy as np
 
+import networkx as nx
 import pandas as pd
+
+from bokeh.models import BoxSelectTool
+from bokeh.models import Circle
+from bokeh.models import HoverTool
+from bokeh.models import MultiLine
+from bokeh.models import TapTool
+from bokeh.models.graphs import EdgesAndLinkedNodes
+from bokeh.models.graphs import NodesAndLinkedEdges
+from bokeh.models.graphs import from_networkx
+from bokeh.palettes import Spectral4
+from bokeh.plotting import figure
 from pathlib import Path
 
-from bokeh.layouts import column
-from bokeh.models import ColumnDataSource
-from bokeh.models import HoverTool
-from bokeh.models import LabelSet
-from bokeh.palettes import Colorblind8
-from bokeh.plotting import curdoc
-from bokeh.plotting import figure
-from bokeh.transform import factor_cmap
+CURR_PATH = Path(__file__).parent
 
-CURR_PATH = Path(__file__).parent()
-
-backgrounds = {
-    'name': CURR_PATH / 'tub-hft-2.og.png',
-    'origin_x': -7.95,
+background = {
+    'name': 'twistmap/tub-hft-3.og.png',
+    'origin_x': -6.59,
     'origin_y': -2.97,
-    'x': 3998,
+    'x': 3862,
     'y': 2243,
     'resolution': 0.01,
 }
 
 nodes = pd.read_csv(CURR_PATH / 'wifi_nodes.csv')
 nodes['floor'] = nodes['room'].str[2].astype(int)
+nodes = nodes[(nodes['floor'] == 3) & (nodes['status'] == 'on')]
+nodes = nodes[nodes['platform'] == 'nuc']
 
-pmap = factor_cmap(
-    'platform',
-    palette=Colorblind8,
-    factors=sorted(nodes.platform.unique()))
+node_positions = nodes.set_index('node_id')[['x', 'y']].to_dict('index')
+for key in node_positions:
+    pos = node_positions[key]
+    node_positions[key] = (pos['x'], pos['y'])
 
 
-def plot_floor(floor=2):
+def create_map():
     p = figure(
         x_range=(-1, 32), y_range=(-1, 16),
         plot_width=320, plot_height=160,
-        tools='hover,save,pan,box_zoom,reset,wheel_zoom',
         toolbar_location='above',
-        title='TWIST TKN floor {}'.format(floor),
+        title='TWIST 3rd floor',
+        sizing_mode='scale_width',
         # output_backend="webgl",
     )
     p.xaxis.axis_label = 'X [m]'
     p.yaxis.axis_label = 'Y [m]'
 
-    p.select_one(HoverTool).tooltips = [
-        ("node", "@node_id"),
-        ("platform", "@platform"),
-        ("position", "@x, @y, @z"),
-    ]
     p.image_url(
-        url=[backgrounds[floor]['name']],
-        x=backgrounds[floor]['origin_x'],
-        y=backgrounds[floor]['origin_y'],
-        w=backgrounds[floor]['x'] * backgrounds[floor]['resolution'],
-        h=backgrounds[floor]['y'] * backgrounds[floor]['resolution'],
+        url=[background['name']],
+        x=background['origin_x'],
+        y=background['origin_y'],
+        w=background['x'] * background['resolution'],
+        h=background['y'] * background['resolution'],
         anchor='bottom_left',
     )
-
-    floor_nodes = nodes[(nodes['floor'] == floor) & (nodes['status'] == 'on')]
-    for platform, color in zip(nodes.platform.unique(), Colorblind8):
-        source = ColumnDataSource(
-            floor_nodes[floor_nodes['platform'] == platform])
-        p.scatter(
-            source=source,
-            x='x', y='y',
-            color=color,
-            size=10,
-            legend=platform,
-        )
-        # labels = LabelSet(
-        #     x='x', y='y', angle=0.6,
-        #     text_font_size="9px",
-        #     text='node_id', level='glyph',
-        #     x_offset=3, y_offset=5, source=source, render_mode='canvas',
-        # )
-        # p.add_layout(labels)
-
-    p.legend.location = "top_right"
-    p.legend.click_policy = 'hide'
-
     return p
 
 
-plots = column([plot_floor(x) for x in range(2, 5)], sizing_mode='scale_width')
-# show(p)  # open a browser
-curdoc().add_root(plots)
-curdoc().title = "TWIST map"
-curdoc()
+def draw_graph(plot, G):
+    plot.add_tools(
+        HoverTool(tooltips=None),
+        TapTool(),
+        BoxSelectTool())
+    graph = from_networkx(G,
+        nx.spring_layout,
+        pos=node_positions,
+        fixed=node_positions.keys())
+
+    graph.node_renderer.glyph = Circle(
+        size=15,
+        fill_color=Spectral4[0])
+    graph.node_renderer.selection_glyph = Circle(
+        size=15,
+        fill_color=Spectral4[2])
+    graph.node_renderer.hover_glyph = Circle(
+        size=15,
+        fill_color=Spectral4[1])
+
+    graph.edge_renderer.glyph = MultiLine(
+        line_color="#CCCCCC", line_alpha=0.8, line_width=5)
+    graph.edge_renderer.selection_glyph = MultiLine(
+        line_color=Spectral4[2], line_width=5)
+    graph.edge_renderer.hover_glyph = MultiLine(
+        line_color=Spectral4[1], line_width=5)
+
+    graph.selection_policy = NodesAndLinkedEdges()
+    graph.inspection_policy = EdgesAndLinkedNodes()
+
+    plot.renderers.append(graph)
+    return plot
