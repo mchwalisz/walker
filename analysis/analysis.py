d5bb6177e44dd5d79ad1b70b5c042f1fdddb6642
Kai Geißdörfer <kai.s.geissdoerfer@campust.tu-berlin.de>
2018-01-15 12:35:11 +0100

    iperf client measurements only

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -9,47 +9,22 @@ from matplotlib.ticker import FuncFormatter
 def get_iperf(source: Path) -> pd.DataFrame:
     with source.open('r') as f:
         raw_data = json.load(f)
-    raw_data_frame = [x['streams'][0] for x in raw_data['intervals']]
-    if not raw_data_frame:
+
+    intervals = [x['streams'][0] for x in raw_data['intervals']]
+    if not intervals:
         raise ValueError('Missing data')
-    df = pd.DataFrame(raw_data_frame)
 
-    try:
-        raw_data_server = [x['streams'][0]
-            for x in raw_data['server_output_json']['intervals']]
-    except KeyError:
-        raise ValueError('Missing data')
-    df_server = pd.DataFrame(raw_data_server)
+    result = pd.DataFrame(intervals)
 
-    df_server.columns = ['server_' + x for x in df_server.columns]
-    result = pd.concat([df, df_server], axis=1)
-    # df_server = df_server.set_index('start')
-
-    server = (raw_data['server_output_json']
-        ['start']['system_info']).split(' ')[1]
-    client = raw_data['start']['system_info'].split(' ')[1]
-    conn = sorted([client, server])
-    result['cookie'] = raw_data['start']['cookie']
-    result['timestamp'] = raw_data['start']['timestamp']['time']
-    result['server'] = server
-    result['system_info'] = raw_data['start']['system_info']
-    # result['server_system_info'] = raw_data['start']['system_info']
+    result['client'] = raw_data['title'].split(' ')[3]
+    result['server'] = raw_data['title'].split(' ')[1]
     result['kernel'] = raw_data['start']['system_info'].split(' ')[2]
-    result['client'] = client
-    result['connection'] = ' '.join(conn)
-    result['traffic'] = raw_data['start']['test_start']['protocol']
-    try:
-        result['title'] = raw_data['title']
-        key_values = raw_data['title'].split(',')
-        if len(key_values) > 2:
-            for kv in key_values:
-                kv = kv.split(':')
-                result[kv[0]] = kv[1]
-    except KeyError:
-        pass
-    result['mode'] = 'Access Point' if conn[0] == server else 'Client'
+
+    result['timestamp'] = raw_data['start']['timestamp']['time']
+    result['system_info'] = raw_data['start']['system_info']
+    result['protocol'] = raw_data['start']['test_start']['protocol']
+
     result['file'] = source.stem
-    result.columns = [x.replace('_', ' ') for x in result.columns]
 
     return result
 
