45b31aaa9a51ec3201fb72fc5afc6882c3852435
Mikołaj Chwalisz <m.chwalisz@gmail.com>
2018-02-07 09:11:43 +0100

    Bump version: 1.0.0 → 1.0.1

--- images/preparation/prepare.py
+++ images/preparation/prepare.py
@@ -94,7 +94,7 @@ def __render_rspec(url):
     multiple=True,
     default=['3.18.87', '4.14.5', '4.15', '4.11.4'],
     help='Version number of mainline kernel to bake into image')
-@click.version_option('v1.0.0')
+@click.version_option('v1.0.1')
 @click.pass_context
 def cli(ctx, diskimage, release, kernel):
     if ctx.invoked_subcommand is None:
